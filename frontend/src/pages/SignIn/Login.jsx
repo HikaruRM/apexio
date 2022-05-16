d02ef8fa94d4479b4edb2d42baa6fa4b73376556
Hikaru.H <41872273+HikaruRM@users.noreply.github.com>
2022-05-16 17:16:01 +0300

    Check pls

--- frontend/src/pages/SignIn/Login.jsx
+++ frontend/src/pages/SignIn/Login.jsx
@@ -7,20 +7,16 @@ function Login() {
   return (
     <>
       <div className={'wrapper'}>
-        <form className={'login-form'}>
+        <form className={'SignInForm'}>
           <div className={'center-wrapper'}>
             <div className={'header'}>
-              <h2>Sign In</h2>
+              <h3 className={"FormH3"}>Sign In</h3>
             </div>
-            {/*<TextInput name={'username'} label={'Username or email'} error={errors.username} defaultValue={encryptedLocalStorage.getItem('username')} />*/}
-            {/*<TextInput type={'password'} name={'password'} label={'Password'} error={errors.password} />*/}
-            {/*<BigButton disabled={!loginEnabled}>Sign In</BigButton>*/}
               <label className={"label"}>Email or Username</label>
               <input className={"input"} type={"text"} placeholder={"Your Email or Username"}/>
               <label className={"label"}>Password</label>
               <input className={"input"} type={"password"} placeholder={"Your Password"}/>
               <button className={"FormButton"}>Sign In</button>
-
             <div>
               <span>Don't have an account? </span>
               <Link className={'link'} to={'/register'}>Sign Up</Link>
