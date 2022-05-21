e0a9ec0a246aa339dc73ca2a0dc110a744136dd3
Hikaru.H <41872273+HikaruRM@users.noreply.github.com>
2022-05-21 21:05:52 +0300

    faq

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -29,7 +29,7 @@ function App() {
           <Route path={"about"} element={<About />} />
           <Route path={"signin"} element={<SignIn />} />
           <Route path={"signup"} element={<SignUp />} />
-          <Route path={"faq"} element={<QandA />} />
+          <Route path={"qanda"} element={<QandA />} />
         </Routes>
       </div>
       <Footer />
--- frontend/src/pages/QandA/QandA.jsx
+++ frontend/src/pages/QandA/QandA.jsx
@@ -5,7 +5,18 @@ function faq() {
     <>
       <h1 className={"title"}>Frequently asked questions</h1>
       <div className={"div-text"}>
-        <p></p>
+        <p className={"Question"}>Why are you the number one store in europe?</p>
+        <p className={"answer"}>Because we said so.</p>
+        <p className={"Question"}>Are your products safe?</p>
+        <p className={"answer"}>86% of the time the products are safe, but there are instances of accessories exploding</p>
+        <p className={"Question"}>What are the shipping fees?</p>
+        <p className={"answer"}>We have two options:</p>
+        <p className={"answer"}>Free shipping, takes up to 365 days to ship</p>
+        <p className={"answer"}>Premium shipping, ships in 2 days and costs 500€</p>
+        <p className={"Question"}>How long did it take to make this website?</p>
+        <p className={"answer"}>Around 2 weeks</p>
+        <p className={"Question"}>What are you using to make this website?</p>
+        <p className={"answer"}>Website is made with react.js, node.js and express.js frameworks</p>
       </div>
     </>
   );
