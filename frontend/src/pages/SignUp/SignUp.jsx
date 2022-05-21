f2c237b3076ca18ae56ec797363217595e80d29a
Hikaru.H <41872273+HikaruRM@users.noreply.github.com>
2022-05-21 19:40:23 +0300

    Another Login Form size update

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -31,13 +31,3 @@ function SignUp() {
 
 export default SignUp;
 
-<form className={"SignUpForm"}>
-  <h3 className={"FormH3"}>Sign Up Here</h3>
-  <label className={"label"}>Email</label>
-  <input className={"input"} type={"text"} placeholder={"Your email"}/>
-  <label className={"label"}>Username</label>
-  <input className={"input"} type={"text"} placeholder={"Your Username"}/>
-  <label className={"label"}>Password</label>
-  <input className={"input"} type={"password"} placeholder={"Your Password"}/>
-  <button className={"FormButton"}>Sign Up</button>
-</form>
