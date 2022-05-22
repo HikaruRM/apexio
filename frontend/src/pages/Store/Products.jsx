a0e7e4ea15f9e4745ad9264cdf87688c3dc76ace
Prada0x <95030704+Viljamsz@users.noreply.github.com>
2022-05-22 22:58:57 +0300

    updated store page

--- frontend/src/pages/Store/Products.jsx
+++ frontend/src/pages/Store/Products.jsx
@@ -1,20 +1,20 @@
 export const products = [
     {
-        name: 'Phone XL',
-        price: 799,
-        description: 'A large phone with one of the best screens',
-        image: 'https://cdn.tet.lv/tetveikals-prd-images/grid_product_image/products/6273608201568855625695-627b7b721beaa.png'
+        name: 'Samsung Odyssey G3',
+        price: 449,
+        description: 'The mid 144Hz monitor on the market with G-SYNC compatibility.',
+        image: 'https://www.rdveikals.lv/images/tumb/c02b11a9e954851dc9530369cd645475.jpg'
     },
     {
-        name: 'Phone Mini',
-        price: 699,
-        description: 'A great phone with one of the best cameras',
+        name: 'Xiaomi Smart Kettle',
+        price: 119,
+        description: 'The smartest kettle you will ever own',
         image: 'https://static.mistore.ge/uploads/products/f9c42644b3fb2b438f3074dbaa8f9bca3fe029418254c60bf1f8f58f501b6242.png'
     },
     {
-        name: 'Phone Standard',
-        price: 299,
-        description: '',
-        image: ''
-    }
+        name: 'Geforce RTX 3090TI',
+        price: 2990,
+        description: 'The power house of the GPUs',
+        image: 'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/RTX_3090_Ti_Render78.jpg'
+    },
 ];
