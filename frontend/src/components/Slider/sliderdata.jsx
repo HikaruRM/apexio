b07eebe0f15876888a20827f0ec67d1f34a853de
Hikaru.H <41872273+HikaruRM@users.noreply.github.com>
2022-05-22 17:28:31 +0300

    Updated gitignore

--- frontend/src/components/Slider/sliderdata.jsx
+++ frontend/src/components/Slider/sliderdata.jsx
@@ -1,8 +1,17 @@
 export const Sliderdata = [
   {
-    image: 'https://www.corsair.com/medias/sys_master/images/images/h82/h60/9686903029790/CH-912A01A-UK/Gallery/K100_RGB_01/-CH-912A01A-UK-Gallery-K100-RGB-01.png_1200Wx1200H'
+    image: 'https://cdn.shopify.com/s/files/1/1265/2145/files/Web_Banner-2_1_1920x914.jpg?v=1651602148'
   },
   {
     image: 'https://cdn.shopify.com/s/files/1/1265/2145/files/slider-CreatorCup-Lolathon_2aab5156-8af9-4252-a0ba-a3bc649477be_1920x914.jpg?v=1653074990'
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/1265/2145/files/Web_Banner_17_cd2874e4-afda-4382-8cf9-2e68c91598be_1920x914.jpg?v=1649330544'
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/1265/2145/files/Web_Banner_1080b902-9aaa-48a5-a9ef-137367fdc52a_1920x914.jpg?v=1652899826'
+  },
+  {
+    image: 'https://cdn.shopify.com/s/files/1/1265/2145/files/Web_Banner_19_1920x914.jpg?v=1652468063'
   }
 ]
