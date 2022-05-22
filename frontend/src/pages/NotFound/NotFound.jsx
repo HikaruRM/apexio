938b8b8535688fb683f48f68078d2662fbf6724a
Prada0x <95030704+Viljamsz@users.noreply.github.com>
2022-05-22 18:28:42 +0300

    Added 404 page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import './NotFound.css';
 const NotFound= () =>{
     return (
         <div className={"div404"}>
-            <h1 className={"title"}>404 Error</h1>
+            <h1 className={"title"}>Error 404</h1>
                 <h3>Sorry, But the page you're looking for doesn't exist.</h3>
         </div>
     );
